7b458de053ee1595e85e65a7050285362c3997ab
Sandeep V <sandeep.v@glowtouch.com>
2020-01-30 17:57:31 +0530

    Added the comments for news apis

--- src/network/NewsProvider.js
+++ src/network/NewsProvider.js
@@ -3,6 +3,7 @@ import * as rssParser from 'react-native-rss-parser';
 const CONSTANTS = require('../utils/Constants');
 
 export default class NewsProvider {
+    // gets the list of top news
     static getTopNews() {
         return fetch(CONSTANTS.BASE_URL)
             .then((response) => response.text())
@@ -12,6 +13,8 @@ export default class NewsProvider {
             });
     }
 
+    // returns the news details for the provided index
+    // since there are no unique id for news, we use news index to get the details
     static getNewsDetail(index) {
         return fetch(CONSTANTS.BASE_URL)
             .then((response) => response.text())
